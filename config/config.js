39cd73381f5540a6db33d7ec88c73ee6228fb5b5
Eric Monterozo <eqmonterozo@findme.com.ph>
2022-07-07 12:32:14 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   SECRET_KEY: "supersecret",
-  HOST: "sql6.freemysqlhosting.net",
-  USER: "sql6504565",
-  PASSWORD: "NV9XCcmMVK",
-  DATABASE: "sql6504565",
+  HOST: "",
+  USER: "",
+  PASSWORD: "",
+  DATABASE: "",
   PORT: 0000,
 };
